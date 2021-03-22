de3682bc3f373771823afdfb821336922003db6f
kl-botsu <botsu@khonsulabs.com>
2021-03-22 02:24:28 +0000

    Deploying to gh-pages from @ khonsulabs/pliantdb@34c1b0c28a549fd0dc9c32e7a470dd6ae8c57d63 🚀

--- main/source-files.js
+++ main/source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["pliantdb"] = {"name":"","dirs":[{"name":"schema","dirs":[{"name":"view","files":["map.rs"]}],"files":["collection.rs","database.rs","document.rs","mod.rs","revision.rs","view.rs"]}],"files":["connection.rs","lib.rs","storage.rs"]};
+sourcesIndex["pliantdb"] = {"name":"","dirs":[{"name":"schema","dirs":[{"name":"view","files":["map.rs"]}],"files":["collection.rs","database.rs","document.rs","mod.rs","revision.rs","transaction.rs","view.rs"]}],"files":["connection.rs","lib.rs","storage.rs"]};
 createSourceSidebar();
