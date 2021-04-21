f71024367696d403280fd442a4d3e8a7fd471409
kl-botsu <botsu@khonsulabs.com>
2021-04-21 19:03:00 +0000

    Deploying to gh-pages from @ khonsulabs/pliantdb@6120fe72a14ba2da665a573918f198257c8f8950 🚀

---
+++ main/implementors/core/convert/trait.Into.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["pliantdb_core"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/alloc/string/struct.String.html\" title=\"struct alloc::string::String\">String</a>&gt; for <a class=\"struct\" href=\"pliantdb_core/schema/struct.Name.html\" title=\"struct pliantdb_core::schema::Name\">Name</a>","synthetic":false,"types":["pliantdb_core::schema::names::Name"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
