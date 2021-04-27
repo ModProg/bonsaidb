48cc4fa8f2e0e5f4ab76e38c9e8ad56cf293f206
kl-botsu <botsu@khonsulabs.com>
2021-04-27 01:57:36 +0000

    Deploying to gh-pages from @ khonsulabs/pliantdb@095f515f0816253fe2ae1fd5c666eb46ee2981ba 🚀

--- main/pliantdb/sidebar-items.js
+++ main/pliantdb/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["cli","`PliantDB` command line tools."],["client","Client for `pliantdb-server`."],["core","Core functionality and types for `PliantDB`."],["local","Local storage backend for `PliantDB`."],["server","The `PliantDB` Server."]]});
+initSidebarItems({"mod":[["cli","`PliantDb` command line tools."],["client","Client for `pliantdb-server`."],["core","Core functionality and types for `PliantDb`."],["local","Local storage backend for `PliantDb`."],["server","The `PliantDb` Server."]]});
