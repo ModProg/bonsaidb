883bda6cf379bc7d412862027a131d7fbe4e7923
kl-botsu <botsu@khonsulabs.com>
2021-11-15 21:52:11 +0000

    Deploying to gh-pages from @ khonsulabs/bonsaidb@273610ca715e62fbc0dcb3cba734d0c67275cdf7 🚀

---
+++ main/implementors/actionable/dispatcher/trait.Dispatcher.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["bonsaidb_server"] = [{"text":"impl Dispatcher&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.56.1/std/primitive.unit.html\">()</a>&gt; for <a class=\"struct\" href=\"bonsaidb_server/struct.NoDispatcher.html\" title=\"struct bonsaidb_server::NoDispatcher\">NoDispatcher</a>","synthetic":false,"types":["bonsaidb_server::backend::NoDispatcher"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
