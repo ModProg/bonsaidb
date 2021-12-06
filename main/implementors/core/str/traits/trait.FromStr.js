95b74940a53d27473641a311a938540f7106e592
kl-botsu <botsu@khonsulabs.com>
2021-12-06 15:12:03 +0000

    Deploying to gh-pages from @ khonsulabs/bonsaidb@a12d0c5d77141dd1bfe7bb1becf74283af236919 🚀

--- main/implementors/core/str/traits/trait.FromStr.js
+++ main/implementors/core/str/traits/trait.FromStr.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["bonsaidb"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.56.1/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"enum\" href=\"bonsaidb/cli/enum.ConnectionTarget.html\" title=\"enum bonsaidb::cli::ConnectionTarget\">ConnectionTarget</a>","synthetic":false,"types":["bonsaidb::cli::ConnectionTarget"]}];
+implementors["bonsaidb"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.57.0/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"enum\" href=\"bonsaidb/cli/enum.ConnectionTarget.html\" title=\"enum bonsaidb::cli::ConnectionTarget\">ConnectionTarget</a>","synthetic":false,"types":["bonsaidb::cli::ConnectionTarget"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
